2cb9e1f3ae7d29f92dc975a4b11788491eba4389
Jasbir Dorai <117107304+callmeasrio@users.noreply.github.com>
2023-03-08 19:25:37 +0530

    Create logo()

---
+++ omniscan.py
@@ -0,0 +1,18 @@
+# logo
+def logo(): {
+    print(
+        "████████  ████   ████  ████     ██  ██  ████████  ████████  ████████  ████     ██\n"
+        "██    ██  ██ ██ ██ ██  ██ ██    ██  ██  ██        ██        ██    ██  ██ ██    ██\n"
+        "██    ██  ██   █   ██  ██  ██   ██  ██  ████      ██        ██    ██  ██  ██   ██\n"
+        "██    ██  ██       ██  ██   ██  ██  ██      ████  ██        ████████  ██   ██  ██\n"
+        "██    ██  ██       ██  ██    ██ ██  ██        ██  ██        ██    ██  ██    ██ ██\n"
+        "████████  ██       ██  ██     ████  ██  ████████  ████████  ██    ██  ██     ████\n")
+}
+
+# install python packages / check for python packages
+# install tools / check for tools
+# list of tools & their default commands
+# option for changes in commands
+# option for selecting tools
+# file_name and directory for output
+logo()
